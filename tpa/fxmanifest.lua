b170162b21f6c2eefecc2d012a724f26d56c172c
1NF1N17Y20 <78461134+1NF1N17Y20@users.noreply.github.com>
2021-03-17 13:46:03 -0400

    Initial Upload

---
+++ tpa/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Resource Metadata
+fx_version "cerulean"
+games {"rdr3", "gta5"}
+
+author "kermit#0001"
+description "1NF1N17Y Development | TPA"
+version "1.0.0"
+
+-- What to run
+client_script "client/cl_main.lua"
+server_script "server/sv_main.lua"
